2ea9113a806e896a8db08acd2fb4100fc97bae95
saradorei <suyang21@hotmail.com>
2018-03-18 18:00:36 -0400

    Add files via upload

---
+++ src/main/java/com/customer/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.customer.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ src/main/java/com/customer/spring/dao/CustomerDAO.java
@@ -0,0 +1,18 @@
+package com.customer.spring.dao;
+
+import java.util.List;
+
+import com.customer.spring.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+	public List<Customer> searchCustomers(String theSearchName);
+}
